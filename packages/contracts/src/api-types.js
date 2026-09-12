d106b342c4b5dc39207b2dcb59f9c5a6f2d95b2f
Tomas Ferreyra <toferreyra@frba.utn.edu.ar>
2026-09-12 01:14:37 -0300

    feat: add package.json for @nfl-games/contracts package

--- packages/contracts/src/api-types.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-/**
- * Auto-generated and maintained API Wire Formats & Domain Contracts
- * Corresponding to OpenAPI 3.1 definitions in apps/api
- */
-Object.defineProperty(exports, "__esModule", { value: true });
